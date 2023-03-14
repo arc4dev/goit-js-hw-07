05112591c1dcc6dcd4246ed2c5d45c685ea7cff2
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-03-14 12:27:26 +0100

    #2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+
+const showImages = (imgs) => {
+  imgs.forEach((img) => {
+    const html = `
+    <a class="gallery__item" href="${img.original}">
+      <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+    </a>`;
+    container.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+showImages(galleryItems);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
